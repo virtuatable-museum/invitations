b42af0f7e44737630e1b2c3bd72ed1afe247fe75
Babausse <courtois.vincent@outlook.com>
2018-04-02 21:50:48 +0200

    delcare the route correctly

--- controllers/invitations.rb
+++ controllers/invitations.rb
@@ -1,6 +1,6 @@
 module Controllers
   class Invitations < Arkaan::Utils::Controller
-    post '/' do
+    declare_route 'post', '/' do
       check_presence('session_id', 'campaign_id', 'account_id')
       account = Arkaan::Account.where(id: params['account_id']).first
       if account.nil?
