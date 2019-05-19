b73c1c939be9e5ca4411131f9fbe971053038619
Babausse <courtois.vincent@outlook.com>
2019-05-19 22:57:10 +0200

    Remove non-existing file from linked files

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,6 @@ set :repo_url, 'git@github.com:jdr-tools/invitations.git'
 set :branch, 'master'
 
 append :linked_files, 'config/mongoid.yml'
-append :linked_files, 'config/buckets.yml'
 append :linked_files, '.env'
 append :linked_dirs, 'bundle'
 append :linked_dirs, 'log'
