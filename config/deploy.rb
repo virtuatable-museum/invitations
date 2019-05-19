de1f22523156570adb50302a3319c3fd7a5e9bf9
Babausse <courtois.vincent@outlook.com>
2019-05-19 22:56:02 +0200

    Prepare the application for Capistrano deployment

---
+++ config/deploy.rb
@@ -0,0 +1,12 @@
+lock '~> 3.11.0'
+
+set :application, 'virtuatable-invitations'
+set :deploy_to, '/var/www/invitations'
+set :repo_url, 'git@github.com:jdr-tools/invitations.git'
+set :branch, 'master'
+
+append :linked_files, 'config/mongoid.yml'
+append :linked_files, 'config/buckets.yml'
+append :linked_files, '.env'
+append :linked_dirs, 'bundle'
+append :linked_dirs, 'log'
