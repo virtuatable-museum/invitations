200d78a2a00319821914e5af5ea2bc6a61de99ae
babausse <courtois.vincent@outlook.com>
2019-08-08 05:57:06 -0700

    Add a primitive Rubocop analysis in the CI, to be expanded later

--- controllers/invitations.rb
+++ controllers/invitations.rb
@@ -1,7 +1,10 @@
+# frozen_string_literal: true
+
 require 'sinatra/custom_logger'
 
 module Controllers
-
+  # Main controller for creating, updating, deleting and listing invitations.
+  # @author Vincent Courtois <courtois.vincent@outlook.com>
   class Invitations < Arkaan::Utils::Controllers::Checked
     helpers Sinatra::CustomLogger
 
@@ -26,9 +29,9 @@ module Controllers
       account = check_account('creation')
       campaign = check_campaign('creation')
 
-      invitation = Services::Invitations::Creation.instance.create(session, campaign, account)
+      invit = create_service.create(session, campaign, account)
 
-      halt 201, {message: 'created', item: Decorators::Invitation.new(invitation).to_h}.to_json
+      halt 201, { message: 'created', item: decorate(invit) }.to_json
     end
 
     declare_route 'get', '/' do
@@ -41,55 +44,77 @@ module Controllers
     declare_route 'put', '/:id' do
       check_presence('status', route: 'update')
 
-      session = check_session('update')
-      invitation = check_invitation('update')
-  
-      invitation = Services::Invitations::Update.instance.update(session, invitation, params['status'])
+      invit = update_service.update(
+        check_session('update'),
+        check_invitation('update'),
+        params['status']
+      )
 
-      halt 200, {message: 'updated', item: Decorators::Invitation.new(invitation).to_h}.to_json
+      halt 200, { message: 'updated', item: decorate(invit) }.to_json
     end
 
     declare_route 'delete', '/:id' do
       session = check_session('deletion')
-      invitation = check_invitation('update')
+      invit = check_invitation('update')
 
-      Services::Invitations::Deletion.instance.delete(session, invitation)
+      delete_service.delete(session, invit)
 
-      halt 200, {message: 'deleted'}.to_json
+      halt 200, { message: 'deleted' }.to_json
     end
 
-    # Checks the presence of the username, and the existence of the associated account.
-    # @param action [String] the action used as a key in the configuration file to find the associated error.
-    # @return [Arkaan::Account] the account if it's been found and has not raised any error.
+    private
+
+    def create_service
+      Services::Invitations::Creation.instance
+    end
+
+    def update_service
+      Services::Invitations::Update.instance
+    end
+
+    def delete_service
+      Services::Invitations::Deletion.instance
+    end
+
+    def decorate(invitation)
+      Decorators::Invitation.new(invitation).to_h
+    end
+
+    # Checks the presence of the username, and the existence of the account.
+    # @param action [String] the action used to find the associated error.
+    # @return [Arkaan::Account] the account if it's been found.
     def check_account(action)
       check_presence('username', route: action)
       account = Arkaan::Account.where(username: params['username']).first
       custom_error(404, "#{action}.username.unknown") if account.nil?
-      return account
+      account
     end
 
     def check_invitation(action)
       invitation = Arkaan::Campaigns::Invitation.where(id: params['id']).first
       custom_error(404, "#{action}.invitation_id.unknown") if invitation.nil?
-      return invitation
+      invitation
     end
 
-    # Checks the presence of the campaign ID, and the existence of the associated campaign.
-    # @param action [String] the action used as a key in the configuration file to find the associated error.
-    # @return [Arkaan::Campaign] the campaign if it's been found and has not raised any error.
+    # Checks the presence of the campaign ID, and the existence of the campaign.
+    # @param action [String] the action used to find the associated error.
+    # @return [Arkaan::Campaign] the campaign if it's been found.
     def check_campaign(action)
       check_presence('campaign_id', route: action)
       campaign = Arkaan::Campaign.where(id: params['campaign_id']).first
       custom_error(404, "#{action}.campaign_id.unknown") if campaign.nil?
-      return campaign
+      campaign
     end
 
     def create_app
       account = Arkaan::Account.where(username: ENV['USERNAME']).first
-      application = Arkaan::OAuth::Application.find_or_create_by(name: 'invitations', premium: true, creator: account)
+      application = Arkaan::OAuth::Application.find_or_create_by(
+        name: 'invitations',
+        premium: true,
+        creator: account
+      )
       application.save
       @oauth_app = application
     end
-
   end
 end
